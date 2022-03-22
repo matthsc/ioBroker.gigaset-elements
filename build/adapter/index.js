9f662711ea2498d781bfd4198467e68e3d29d374
Matthias Schwende <matthsc@gmx.net>
2022-03-22 12:31:07 +0100

    remove build directories

--- build/adapter/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-(0, tslib_1.__exportStar)(require("./basestation"), exports);
-(0, tslib_1.__exportStar)(require("./elements"), exports);
-(0, tslib_1.__exportStar)(require("./events"), exports);
-(0, tslib_1.__exportStar)(require("./messageHandler"), exports);
-//# sourceMappingURL=index.js.map
