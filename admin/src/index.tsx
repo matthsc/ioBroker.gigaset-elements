b797dc21475e5aaa8367c64b88d6a61f8c0a875c
Matthias Schwende <matthsc@gmx.net>
2022-09-28 14:31:57 +0200

    downgrade to react@17

--- admin/src/index.tsx
+++ admin/src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import { render } from "react-dom";
 import { ThemeProvider, StyledEngineProvider } from "@mui/material/styles";
 import theme from "@iobroker/adapter-react-v5/Theme";
 import Utils from "@iobroker/adapter-react-v5/Components/Utils";
@@ -8,8 +8,7 @@ import App from "./app";
 let themeName = Utils.getThemeName();
 
 function build(): void {
-    const root = createRoot(document.getElementById("root") as HTMLElement);
-    root.render(
+    render(
         <StyledEngineProvider injectFirst>
             <ThemeProvider theme={theme(themeName)}>
                 <App
@@ -21,6 +20,7 @@ function build(): void {
                 />
             </ThemeProvider>
         </StyledEngineProvider>,
+        document.getElementById("root") as HTMLElement,
     );
 }
 
